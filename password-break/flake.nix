f25a25552b1e27543700e0f875a68988ebf99ded
rafal <you@example.com>
2026-03-24 11:07:25 +0100

    chore: init

---
+++ password-break/flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+
+          buildInputs = with pkgs; [
+            dotnet-sdk_10
+          ];
+        };
+      }
+    );
+}
